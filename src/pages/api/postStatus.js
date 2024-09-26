814d8099d97a03001f2602f9af4166c6e4d7ca60
devbadalsingh <badal@only1loan.com>
2024-09-26 02:08:04 -0700

    Make the page dynamic

---
+++ src/pages/api/postStatus.js
@@ -0,0 +1,19 @@
+// import Ticket from "@/models/TicketInfo";
+
+// export default function addStatusToUser (req,res){
+//     try {
+
+//         const { userId } = req.params;
+
+
+//         const addthesatus = Ticket.sa
+
+//     } catch (error) {
+//         return (
+//             res.status(500).json({
+//                 success :false,
+//                 message : `Error in seeting the status ${error.message}`
+//             })
+//         )
+//     } 
+// }
